f7212fd79340bde5a828fd654124fbc30b928796
GuzmanDimas <67974116+dimasguzman@users.noreply.github.com>
2020-07-22 17:32:08 -0600

    Add files via upload

---
+++ modulo7.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.modulo7;
+
+import java.awt.Panel;
+
+
+
+/**
+ *
+ * @author Dimas
+ */
+public class modulo7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Panel convertidor= new Panel();
+     convertidor.setVisible(true);
+
+    }
+    
+}
